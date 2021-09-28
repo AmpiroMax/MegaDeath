613de31b18cc1048811ab925d1d1dad59ea6d581
EvgrafovMichail <64869411+EvgrafovMichail@users.noreply.github.com>
2021-09-28 14:23:51 +0300

    add tile

---
+++ drafts/code/tile/tile.cpp
@@ -0,0 +1,32 @@
+#include "tile.h"
+
+
+Tile::Tile(std::uint16_t tile):
+	_tile(tile) {}
+
+
+Tile::Tile(): Tile(0x0000) {}
+
+
+bool Tile::isKnown() const
+{
+	return (_tile & KNOWLEDGE_MASK);
+}
+
+
+bool Tile::isPassible() const
+{
+	return (_tile & PASSABILITY_MASK);
+}
+
+
+std::uint16_t Tile::getTextureId() const
+{
+	return ((_tile & TEXTURE_ID_MASK) >> 5);
+}
+
+
+std::uint16_t Tile::getIlluminationLevel() const
+{
+	return ((_tile & ILLUMINATION_MASK) >> 2);
+}
