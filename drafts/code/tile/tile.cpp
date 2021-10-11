f7604b5df1b99c0d92214e241f152faf112a357e
EvgrafovMichail <64869411+EvgrafovMichail@users.noreply.github.com>
2021-10-11 23:09:10 +0300

    add documentation

--- drafts/code/tile/tile.cpp
+++ drafts/code/tile/tile.cpp
@@ -13,24 +13,56 @@ Tile::Tile(): Tile(NIL) {}
 
 
 void Tile::setKnowledgeBit(bool knowlege)
+/*
+	Эта функция устанавливает значения бита, 
+	отвечающего за разведонность территории
+
+	:param: bool knowlege - true - если тайл разведан
+
+	:return: ничего
+*/
 {
 	(knowlege) ? _tile |= KNOWLEDGE_MASK : _tile &= (~KNOWLEDGE_MASK);
 }
 
 
 void Tile::setPassabilityBit(bool passible)
+/*
+	Эта функция устанавливает значения бита, 
+	отвечающего за проходимость территории
+
+	:param: bool passible - true - если тайл проходим
+
+	:return: ничего
+*/
 {
 	(passible) ? _tile |= PASSABILITY_MASK : _tile &= (~PASSABILITY_MASK);
 }
 
 
 void Tile::setTextureId(std::uint16_t id)
+/*
+	Эта функция устанавливает значения битов, 
+	кодирующих номер ID текстуры
+
+	:param: std::uint16_t id - id текстуры(от 0 до 1023)
+
+	:return: ничего
+*/
 {
 	_replaceGroupOfBits(TEXTURE_ID_MASK, id, MAX_ID, TEXTURE_SHIFT);
 }
 
 
 void Tile::setIlluminationLevel(std::uint16_t level)
+/*
+	Эта функция устанавливает значения битов, 
+	кодирующих уровень освещённости
+
+	:param: std::uint16_t level - уровень освещённости(от 0 до 7)
+
+	:return: ничего
+*/
 {
 	_replaceGroupOfBits(ILLUMINATION_MASK, level, 
 		MAX_ILLUMINATION, ILLUMINATION_SHIFT);
@@ -38,24 +70,56 @@ void Tile::setIlluminationLevel(std::uint16_t level)
 
 
 bool Tile::isKnown() const
+/*
+	Эта функция позволяет получить информацию
+	о разведанности территории
+
+	:param: нет параметров
+
+	:return: true, если территория разведана
+*/
 {
 	return (_tile & KNOWLEDGE_MASK);
 }
 
 
 bool Tile::isPassible() const
+/*
+	Эта функция позволяет получить информацию
+	о проходимости территории
+
+	:param: нет параметров
+
+	:return: true, если территория проходима
+*/
 {
 	return (_tile & PASSABILITY_MASK);
 }
 
 
 std::uint16_t Tile::getTextureId() const
+/*
+	Эта функция позволяет получить ID текстуры,
+	накладываемой на данный тайл
+
+	:param: нет параметров
+
+	:return: id текстуры - число от 0 до 1023
+*/
 {
 	return ((_tile & TEXTURE_ID_MASK) >> TEXTURE_SHIFT);
 }
 
 
 std::uint16_t Tile::getIlluminationLevel() const
+/*
+	Эта функция позволяет получить уровень
+	освещённости на данном тайле
+
+	:param: нет параметров
+
+	:return: уровень освещённости - число от 0 до 7
+*/
 {
 	return ((_tile & ILLUMINATION_MASK) >> ILLUMINATION_SHIFT);
 }
@@ -63,6 +127,17 @@ std::uint16_t Tile::getIlluminationLevel() const
 
 void Tile::_replaceGroupOfBits(std::uint16_t targetBits, std::uint16_t newValue,
 	std::uint16_t maxValue, std::uint16_t shift)
+/*
+	Вспомогательная функция, для замены определённых битов
+
+	:param: std::uint16_t targetBits - битовая маска, для получения нужных битов
+	:param: std::uint16_t newValue - новое значение заменяемых битов
+	:param: std::uint16_t maxValue - ограничение на новое значение
+	:param: std::uint16_t shift - значение сдвига для нового значения, 
+					необходимо, чтобы заменить нужные биты
+
+	:return: ничего
+*/
 {
 	assert(newValue <= maxValue, ":ERROR: maximum value is exceeded");
 
--- drafts/code/tile/tile_constants.h
+++ drafts/code/tile/tile_constants.h
@@ -5,18 +5,18 @@
 #include <cstdint>
 
 
-constexpr std::uint16_t NIL = 0;
+constexpr std::uint16_t NIL = 0;					// двухбайтовый ноль
 
-constexpr std::uint16_t TEXTURE_SHIFT = 5;
-constexpr std::uint16_t ILLUMINATION_SHIFT = 2;
+constexpr std::uint16_t TEXTURE_SHIFT = 5;			// значение сдвига для получения ID текстуры
+constexpr std::uint16_t ILLUMINATION_SHIFT = 2;		// значение сдвига для получения освещённости
 
-constexpr std::uint16_t MAX_ILLUMINATION = 7;
-constexpr std::uint16_t MAX_ID = 0x03FF;
+constexpr std::uint16_t MAX_ILLUMINATION = 7;		// максимальное значение освещённости
+constexpr std::uint16_t MAX_ID = 0x03FF;			// максимальное значение ID
 
-constexpr std::uint16_t PASSABILITY_MASK = 0x8000;
-constexpr std::uint16_t TEXTURE_ID_MASK = 0x7FE0;
-constexpr std::uint16_t ILLUMINATION_MASK = 0x001C;
-constexpr std::uint16_t KNOWLEDGE_MASK = 0x0002;
+constexpr std::uint16_t PASSABILITY_MASK = 0x8000;	// маска для получения бита проходимости
+constexpr std::uint16_t TEXTURE_ID_MASK = 0x7FE0;	// маска для получения битов ID текстуры
+constexpr std::uint16_t ILLUMINATION_MASK = 0x001C;	// маска для получения уровня освещённости
+constexpr std::uint16_t KNOWLEDGE_MASK = 0x0002;	// маска для получения бита разведонности территории
 
 
 #endif // !TILE_CONSTANTS
