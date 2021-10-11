975a86173e5baf0658c64e99e0f2dcd927ca38ce
EvgrafovMichail <64869411+EvgrafovMichail@users.noreply.github.com>
2021-10-11 23:12:12 +0300

    add documentation

--- drafts/code/tile/tile_constants.h
+++ drafts/code/tile/tile_constants.h
@@ -5,13 +5,13 @@
 #include <cstdint>
 
 
-constexpr std::uint16_t NIL = 0;					// двухбайтовый ноль
+constexpr std::uint16_t NIL = 0;			// двухбайтовый ноль
 
-constexpr std::uint16_t TEXTURE_SHIFT = 5;			// значение сдвига для получения ID текстуры
+constexpr std::uint16_t TEXTURE_SHIFT = 5;		// значение сдвига для получения ID текстуры
 constexpr std::uint16_t ILLUMINATION_SHIFT = 2;		// значение сдвига для получения освещённости
 
 constexpr std::uint16_t MAX_ILLUMINATION = 7;		// максимальное значение освещённости
-constexpr std::uint16_t MAX_ID = 0x03FF;			// максимальное значение ID
+constexpr std::uint16_t MAX_ID = 0x03FF;		// максимальное значение ID
 
 constexpr std::uint16_t PASSABILITY_MASK = 0x8000;	// маска для получения бита проходимости
 constexpr std::uint16_t TEXTURE_ID_MASK = 0x7FE0;	// маска для получения битов ID текстуры
