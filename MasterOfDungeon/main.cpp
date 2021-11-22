b757041bbdfba513cbbe61f4efa3a17021391b8c
EvgrafovMichail <64869411+EvgrafovMichail@users.noreply.github.com>
2021-11-22 23:44:30 +0300

    test merge

--- MasterOfDungeon/main.cpp
+++ MasterOfDungeon/main.cpp
@@ -1,8 +1,12 @@
 #include "Game/game.h"
 
+//комментарий от EvgrafovMichail
+
 int main(int argc, char *argv[])
 {
     Game game(argc, argv);
     game.gameLoop();
     return 0;
 }
+
+//Комментарий после main() от EvgrafovMichail
