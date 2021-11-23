11b1f091d14549f163714f542b4d8731f10695e3
AmpiroMax <patrat2015@yandex.ru>
2021-11-23 12:32:00 +0300

    Dumb commit deleted

--- MasterOfDungeon/main.cpp
+++ MasterOfDungeon/main.cpp
@@ -1,13 +1,8 @@
 #include "Game/game.h"
 
-// Комментарий от Ampiro для проверки merge
-// комментарий от EvgrafovMichail
-
 int main(int argc, char *argv[])
 {
     Game game(argc, argv);
     game.gameLoop();
     return 0;
 }
-
-// Комментарий после main() от EvgrafovMichail
